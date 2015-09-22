644af5ab6c8f94d370e52c96385c0bcb94412acc
Petronetto <juliano@petronetto.com.br>
2015-09-22 01:31:14 -0300

    Editions finished

--- index.php
+++ index.php
@@ -3,4 +3,9 @@
 define('WWW_ROOT', dirname(__FILE__));
 define('DS', DIRECTORY_SEPARATOR);
 
-require_once 'autoload.php';
+require_once(WWW_ROOT . DS . 'autoload.php');
+
+use Classes\Foo;
+$foo = new Foo();
+
+$bar = new Classes\Bar(); //Instancing directly
